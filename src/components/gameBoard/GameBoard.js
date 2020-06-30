4d4335c8cb4df88fc86c2d2030572e2fc518c6c6
crashdaddy <kennethscoggins@mail.com>
2020-06-30 02:27:44 -0500

    rebuilt game engine to use arrays instead of DOM

--- src/components/gameBoard/GameBoard.js
+++ src/components/gameBoard/GameBoard.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 import Tile from './Tile';
-import { moveDown, moveLeft, moveRight, moveUp, checkWin } from './Moves';
-
 
 class GameBoard extends Component {
   constructor(props) {
@@ -9,7 +7,6 @@ class GameBoard extends Component {
 
     this.state = {
       board: [],
-      backgroundPos: this.props.solvedBoard,
       firstClick: false,
       selectedTile: '',
       clickedRow: '',
@@ -23,19 +20,19 @@ class GameBoard extends Component {
     if (this.state.firstClick) {
       let rowCol = id.split('-');
       if (rowCol[0] < this.state.clickedRow) {
-        moveUp(id, this.props.width);
+        this.props.colUp(this.props.board,id)
         this.props.countMove();
       };
       if (rowCol[0] > this.state.clickedRow) {
-        moveDown(id, this.props.width);
+        this.props.colDown(this.props.board,id);
         this.props.countMove();
       };
       if (rowCol[1] > this.state.clickedCol) {
-        moveRight(id, this.props.width);
+        this.props.rowRight(this.props.board,id);
         this.props.countMove();
       };
       if (rowCol[1] < this.state.clickedCol) {
-        moveLeft(id, this.props.width);
+        this.props.rowLeft(this.props.board,id);
         this.props.countMove();
       };
       this.clearAll();
@@ -48,9 +45,7 @@ class GameBoard extends Component {
         clickedCol: rowCol[1]
       })
     }
-    if (checkWin(this.props.width, this.props.cheatMode, this.props.solvedBoard)) {
-      this.props.gameOver();
-    }
+ 
   }
 
   clearAll = () => {
@@ -89,7 +84,7 @@ class GameBoard extends Component {
 
     return (
       <div>
-        {this.props.board.map((tile, idx) => <Tile className="tile" id={this.props.indexBoard[idx]} key={idx} tile={tile.tile} size={tile.size} bgImg={this.props.bgImg} pos={tile.pos} numberPosition={tile.numberPosition} selected={tile.selected} clearSelected={() => this.clearSelected(tile.tile, this.props.indexBoard[idx])} />)}
+        {this.props.board.map((tile, idx) => <Tile className="tile" cheatMode={this.props.cheatMode} id={this.props.indexBoard[idx]} key={idx} tile={tile} bgImg={this.props.bgImg} clearSelected={() => this.clearSelected(tile.tile, this.props.indexBoard[idx])} />)}
       </div>
     )
   }
