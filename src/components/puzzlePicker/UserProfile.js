8171fb02c6043f55c3dffff30e31f89a2d338dcf
crashdaddy <kennethscoggins@mail.com>
2020-07-14 02:21:43 -0500

    made search profile look zexxy

--- src/components/puzzlePicker/UserProfile.js
+++ src/components/puzzlePicker/UserProfile.js
@@ -1,21 +1,41 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
 import Paper from '@material-ui/core/Paper';
+import money from '../img/moneyBag.png';
 
 class UserProfile extends Component {
 
 
     render() {
-        
-        return (
-            <div className="puzzlePickerResultsLayout">
-                {this.props.user && this.props.user.map((userprofile, idx) =>
-                    <Paper key={idx} className="puzzlePickerDiv" elevation={3} >
-                        <Link to={`/profile/${userprofile.user_name}`} ><img className="profilePicLarge" alt='' src={`https://robohash.org/${userprofile.user_name}.png`} /></Link>
-                        <div style={{ width: '90%', fontSize: 'small', fontWeight: 'bold', margin: '2px auto', padding: '5px' }}>{userprofile.user_name}</div>
 
-                    </Paper>)}
-                    <span style={{fontSize:'80px'}}>himom</span>
+        return (
+            <div style={{width:'100%',marginTop:'50px',backgroundColor:'white'}}>
+                {this.props.user.length>0 ?
+                    <div style={{display:'flex',flexDirection:'row',justifyContent:'space-around'}}>
+                        {this.props.user.map((userprofile, idx) =>
+                        <Link to={`/profile/${userprofile.user_name}`} >
+                            <Paper key={idx} className="profileSearchResults" elevation={3} >
+                                <div>
+                                <img className="profilePicLarge" alt='' src={`https://robohash.org/${userprofile.user_name}.png`} style={{width:'50px',height:'50px',borderRadius:'50%',border:'1px solid black'}}/>
+                                <div style={{ width: '90%', fontSize: 'small', fontWeight: 'bold', margin: '2px auto', padding: '5px' }}>{userprofile.user_name}</div>
+                                </div>
+                                <div>
+                                    <img src={money} style={{verticalAlign:'middle',width:'32px',marginBottom:'10px'}} /><span style={{fontWeight:'bold',color:'green'}}> ${userprofile.totalScore ? userprofile.totalScore : 0} </span>
+                                </div>
+                                <div>
+                                   Puzls Unpuzld: <span style={{color:'blue',fontWeight:'bold'}}>{userprofile.gamesPlayed}</span> 
+                                </div>
+                                <div>
+                                   Records Won! <span style={{color:'blue',fontWeight:'bold'}}>{userprofile.recordsWon}</span> 
+                                </div>
+                            </Paper>
+                            </Link>
+                            )
+                        }
+                    </div>
+                    :
+                    <div style={{ fontSize: '80px' }}>Loading...</div>
+                }
             </div>
         )
     }
