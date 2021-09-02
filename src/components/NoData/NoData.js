76cee22a95705fca6da0396b96eb1e535fd6f799
crashdaddy <kennethscoggins@mail.com>
2021-09-02 06:19:56 -0500

    added stick around msg to NoData component

--- src/components/NoData/NoData.js
+++ src/components/NoData/NoData.js
@@ -8,7 +8,7 @@ function NoData() {
             The API that provides us with new and original photos has a rate limit of 5,000 requests per hour, which we seem to have exceeded.
             <p />Help us fund increased API rate limits!<br />
             <p style={{fontSize:'small'}}>If this is the first tine you're seeing this message, please wait a minute for the data feed to connect</p>
-            <p />
+            <p /> 
             <a href="https://www.ko-fi.com/crashdaddy" target="blank"><img src={kofi} alt='' style={{ maxWidth: '250px' }} /></a>
         </div>
     )
